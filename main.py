1c09ba22d49736a01d58d179f6b72aecfdca8b0a
Roman <RomanAl3001@yandex.ru>
2021-09-29 15:38:38 +0300

    sys info

--- main.py
+++ main.py
@@ -10,13 +10,42 @@ def print_hi(name):
 def sysinfo():
 
     print('************* системная информация *******************')
-    print(os.name)
-    print(os.environ)
+    print("Тип ОС: "+str(os.name))
+    print("Общая информация: "+str(os.environ))
     # print(os.path.getsize("c:"))
     totalsize = psutil.disk_usage('C:').total / 2 ** 30
-    print('totalsize: ', totalsize, ' GB')
     free = psutil.disk_usage("c:").free / (1024 * 1024 * 1024)
-    print(f"{free:.4} Gb free on disk {'C:'}")
+    print('Данные диска: ' + str(psutil.disk_usage('c:')))
+    print('Размер диска: ', f"{totalsize:.4}", 'GB,  ',f"{free:.4} Gb свободно на диске: {'C:'}")
+    print('Системная статистика диска: ' + str(psutil.disk_io_counters('c:')))
+    # print(f"{free:.4} Gb free on disk {'C:'}")
+
+    print('Статистика процессора: (кол переключений, прерываний, прог прерываний, сис вызовов) :' + str(psutil.cpu_stats()))
+    # print('Температура процессора: '+str(psutil.sensors_temperatures()))
+    # print('Вентилятор: '+str(psutil.sensors_fans()))
+    print('Время автономной работы батареи: ' + str(psutil.sensors_battery()))
+    print('Затраченое время процессора (секунд) на пользователя, сис процессы, простоя, апарат прерыв, отл вызовы : ' + str(psutil.cpu_times()))
+    print('Загрузка процессора (%): ' + str(psutil.cpu_percent(interval=1))) # есть нюансы!
+    print('Количество ядер процессора (шт.): ' + str(psutil.cpu_count(logical=False)))
+    print('Виртуальная память: ' + str(psutil.virtual_memory()))
+    print('Статистика подкачки памяти: ' + str(psutil.swap_memory()))
+
+
+    print('Статистика сетевого вводв вывода: ' + str(psutil.net_io_counters()))
+    print('Общесистемные соединения сокетов: ' + str(psutil.net_connections()))
+    print('Адреса сетевой карты: ' + str(psutil.net_if_addrs()))
+    print('Информация о сетевых картах: ' + str(psutil.net_if_stats()))
+
+    # print('Время загрузки системы (???): ' + str(psutil.bot_time()))
+    print('Подключенные пользователи: ' + str(psutil.users()))
+    print('Запущеные PID (id процессов): ' + str(psutil.pids()))
+    print('Запущенные процессы:', end=' ')
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
+              print(proc.info, end=' ')
+    print(' ')
+    print('Соединения сокитов: ' + str(psutil.net_connections()))
+
+    print(': ' + str(psutil.Process().open_files()))
 
 def lookmf(d, dn):
 
@@ -85,7 +114,7 @@ def start():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
-    start()
-
+    # start()
+    sysinfo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
